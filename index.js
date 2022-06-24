c03a3825e7af726c7b6072da74f1d2f2aeb14aa2
Carlos Jeurissen <1038267+CarlosJeurissen@users.noreply.github.com>
2022-06-24 14:02:48 +0200

    Update to v0.4.8, update dependencies, update codestyle

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 'use strict';
 
 const fs = require('fs');
@@ -11,18 +12,18 @@ const usedAsCli = executionPath.endsWith('/zip-self') || executionPath.endsWith(
 function getGlobsToInclude (excludeGlobs) {
   const globs = ['**'];
   if (Array.isArray(excludeGlobs)) {
-    excludeGlobs.forEach(function (item) {
+    excludeGlobs.forEach((item) => {
       globs.push('!' + item);
     });
   }
 
   return globby(globs, {
-    gitignore: true
+    gitignore: true,
   });
 }
 
 function writeToArchive (outputPath, globs) {
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     const outputStream = fs.createWriteStream(outputPath);
 
     outputStream.on('close', resolve);
@@ -31,7 +32,7 @@ function writeToArchive (outputPath, globs) {
     archive.on('error', reject);
     archive.pipe(outputStream);
 
-    globs.forEach(function (glob) {
+    globs.forEach((glob) => {
       archive.glob(glob);
     });
     archive.finalize();
@@ -59,9 +60,7 @@ function generate (params) {
   const outputPath = params.outputPath.replace('{version}', version);
   const excludeGlobs = params.excludeGlobs;
 
-  return getGlobsToInclude(excludeGlobs).then(function (globs) {
-    return writeToArchive(outputPath, globs);
-  }).then(function () {
+  return getGlobsToInclude(excludeGlobs).then((globs) => writeToArchive(outputPath, globs)).then(() => {
     console.log(outputPath + ' is ready.');
   }, console.error);
 }
@@ -80,7 +79,7 @@ if (usedAsCli) {
 
   generate({
     outputPath: outputPath,
-    excludeGlobs: excludeGlobs
+    excludeGlobs: excludeGlobs,
   });
 }
 
